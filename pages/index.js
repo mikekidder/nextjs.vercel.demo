a128bbdeb534063679507e15dba353f2b74660ea
Mike Kidder <mikekidder@gmail.com>
2021-04-02 12:27:16 -0600

    Added about page

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Deploy! &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
