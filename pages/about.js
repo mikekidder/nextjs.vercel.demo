553af9b10b83240bc7db7dcb9d487ff0c01ec8ee
Mike Kidder <mikekidder@gmail.com>
2021-04-02 12:28:58 -0600

    add the page, and npm lock file

---
+++ pages/about.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function About() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>CRA on Next.js - About</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          This is the About page.
+        </h1>
+
+        <p className={styles.description}>
+          It doesn't get much{' '}
+          <code className={styles.code}>THAN THIS!</code>
+        </p>
+
+       </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
